f0ee9b91ad43d4628b63809c4e05aa23dc526e46
Ih4teappels <kev10@live.nl>
2015-10-13 14:22:58 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -11,19 +11,7 @@ setlocale(LC_ALL, 'nl_NL');
 
 // databasegegevens
 
-    if($_SERVER['HTTP_HOST'] == 'localhost'){
-        define('DB_HOST','localhost');
-        define('DB_NAME','myBand');
-        define('DB_USERNAME','root');
-        define('DB_PASSWORD','');
-    } else {
-        define('DB_HOST','127.0.0.1');
-        define('DB_NAME','17786_myband');
-        define('DB_USERNAME','kevinm97');
-        define('DB_PASSWORD','mybandpass');
-        //kevinm97
-        //mybandpass
-    }
+    
 
     define('NR_ITEMS_PAGE',3);
 
